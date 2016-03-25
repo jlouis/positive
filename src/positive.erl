ed03605fc545fa694ceb781bb57b1c4abc74e7f6
Heinz N. Gies <heinz@licenser.net>
2016-03-25 04:43:55 +0100

    fix typo

--- src/positive.erl
+++ src/positive.erl
@@ -2,7 +2,7 @@
 -module(positive).
 -export([is_positive/1, 'really?'/1]).
 
-%% @doc Checks weather an integer is posirive or not.
+%% @doc Checks whether an integer is positive or not.
 -spec is_ineeger(any()) -> boolean().
 is_positive(N) when is_integer(N) ->
     N > 0;
@@ -15,7 +15,7 @@ is_positive(_) -> false.
 
 -spec 'perhaps?'(any()) -> true.
 %% @doc Optimized version of 'really?'/1 that uses heuristics
-%% only use this when you are not 'really?' conceared about weather
+%% only use this when you are not 'really?' conceared about whether
 %% a value is positive but just want to know if it 'perhaps?' is.
 'perhaps?'(_N) when is_integer(_N) ->
     true;
